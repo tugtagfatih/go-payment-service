0d12aa13242fc986f021527f46c316a4d0f2bf56
tugtagfatih <tugtagfatih@gmail.com>
2025-10-11 00:38:38 +0000

    admin page edits have been made

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -5,7 +5,7 @@
   channel = "stable-24.05"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    # pkgs.go
+    pkgs.go
     # pkgs.python311
     # pkgs.python311Packages.pip
     # pkgs.nodejs_20
